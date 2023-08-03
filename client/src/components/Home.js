1f91842fa6b8501ddfd156b4a035ec15c4381e03
imkozin <ivan.kozin@hotmail.com>
2023-08-03 12:20:04 +0300

    created navigation

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import { TextField } from "@mui/material";
+
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <TextField id="book" label="Book Title or Author" variant="outlined" />
+        </>
+    )
+}
+
+export default Home;
