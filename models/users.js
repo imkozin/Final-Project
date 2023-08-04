447dbe2c803b21e07ecb4978e14778c564db79e2
imkozin <ivan.kozin@hotmail.com>
2023-08-05 01:12:17 +0300

    added user schema

--- models/users.js
+++ models/users.js
@@ -1,9 +1,22 @@
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-    userName: String,
-    email: String,
-    password: String
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
 });
 
 const User = model('User', userSchema);
