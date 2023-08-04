20bb02557f8711f6500cede038db122f25729119
imkozin <ivan.kozin@hotmail.com>
2023-08-05 01:33:04 +0300

    setting up MongoDB

--- config/db.js
+++ config/db.js
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose';
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-})
 
 const db = mongoose.connection
 
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
+
+export const start = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to the MongoDB database');
+    } catch (e) {
+        console.error('Error connecting to the database', e);
+    }
+}
