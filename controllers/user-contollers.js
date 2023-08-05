cd339c7ea2a2f57ffbeac351e6d8c7640905cd00
imkozin <ivan.kozin@hotmail.com>
2023-08-05 17:27:32 +0300

    added users

--- controllers/user-contollers.js
+++ controllers/user-contollers.js
@@ -2,17 +2,18 @@ import User from "../models/user-models.js";
 import bcrypt from 'bcrypt';
 
 export const register = async (req, res) => {
-    const {name, email, password} = req.body;
-    const lower_email = email.toLowerCase();
-    try {
+        const {name, email, password} = req.body;
+        const lower_email = email.toLowerCase();
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
+    try {
         const newUser = new User({
             name,
             email : lower_email,
             password: hash 
-        }).save();
-        return newUser;
+        });
+        const savedUser = await newUser.save();
+        res.status(200).json({user: savedUser, msg: "Registered successfully"})
     } catch (error) {
         res.status(409).json({ msg: "User already exists" });
     }
